98cd695cf5208b17826d78c2f578b4f30bdd42dc
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-10-03 00:13:55 +0200

    Versión 1.0

--- src/Aquarium.kt
+++ src/Aquarium.kt
@@ -1,13 +1,18 @@
-class Aquarium (
-    var length: Int = 100,
-    var width: Int = 20,
-    var height: Int = 40
+open class Aquarium (
+    open var length: Int = 100,
+    open var width: Int = 20,
+    open var height: Int = 40
 ) {
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
 
     constructor(numberOfFish: Int): this() {
         val tank = numberOfFish * 2000 * 1.1
@@ -15,11 +20,13 @@ class Aquarium (
     }
 
     fun printSize() {
+        println(shape)
         println(
             "Width: $width cm, " +
             "Length: $length cm, " +
             "Height: $height cm, "
         )
-        println("Volume: $volume liters")
+        println("Volume: $volume liters; Water: $water liters (${water / volume * 100.0}% full)")
+
     }
 }
--- src/Leccion3.kt
+++ src/Leccion3.kt
@@ -13,6 +13,11 @@ fun buildAquarium() {
 
     val aquarium5 = Aquarium(numberOfFish = 29)
     aquarium5.printSize()
+    aquarium5.volume = 70
+    aquarium5.printSize()
+
+    val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
+    aquarium6.printSize()
 }
 
 fun main() {
