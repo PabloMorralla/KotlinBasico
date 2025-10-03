507716d4eff46da9712f71ab223618e2239a17fb
Pablo Morralla <pabmordel2@alu.edu.gva.es>
2025-10-03 16:53:15 +0200

    Versión 2.0 (acabado)

--- src/Leccion3.kt
+++ src/Leccion3.kt
@@ -1,3 +1,11 @@
+fun main() {
+    buildAquarium()
+    println("----------------------------------")
+    makeFish()
+    println("----------------------------------")
+    makeDecorations()
+}
+
 fun buildAquarium() {
     val aquarium1 = Aquarium()
     aquarium1.printSize()
@@ -18,8 +26,46 @@ fun buildAquarium() {
 
     val aquarium6 = Aquarium(length = 25, width = 25, height = 40)
     aquarium6.printSize()
+
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
-fun main() {
-    buildAquarium()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+    println("Shark: ${shark.color}")
+    shark.eat()
+
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun makeDecorations() {
+    val decoration1 = Decoration("granite")
+    println(decoration1)
+
+    val decoration2 = Decoration("slate")
+    println(decoration2)
+
+    val decoration3 = Decoration("slate")
+    println(decoration3)
+
+    println(decoration1.equals(decoration2))
+    println(decoration3.equals(decoration2))
+
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+
+    println(Color.RED.name)
+    println(Color.GREEN.ordinal)
+    println(Color.BLUE.rgb)
 }
--- src/TowerTank.kt
+++ src/TowerTank.kt
@@ -1,15 +1,19 @@
 import java.lang.Math.PI
 
 class TowerTank (
-    override var height: Int, var diameter: Int
+    override var height: Int,
+    var diameter: Int
 ): Aquarium (
     height = height,
     width = diameter,
     length = diameter
 ) {
     override var volume: Int
-        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt() // area = pi * r1 * r2
         set (value) {
             height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
         }
+
+    override var water = volume * 0.80
+    override val shape = "cylinder"
 }
